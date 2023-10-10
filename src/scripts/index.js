ba93a731310fe8dc25760a31ecec40080ffd8934
Ian Septiana <insaidseptiana@gmail.com>
2023-10-10 13:15:23 +0700

    fix data source, Aksesibilitas Website 44x44px

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,21 +1,66 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
+import DataSource from '../public/data/data-source';
+import { async } from 'regenerator-runtime';
 
-const menuBtn = document.querySelector('.menu-btn');
-const hero = document.querySelector('.hero');
-const main = document.querySelector('main');
-const drawer = document.querySelector('#drawer');
+document.addEventListener('DOMContentLoaded', () => {
 
-menuBtn.addEventListener('click', function (event) {
-  drawer.classList.toggle('active');
-  event.stopPropagation();
-});
+  const menuBtn = document.querySelector('.menu-btn');
+  const hero = document.querySelector('.hero');
+  const main = document.querySelector('main');
+  const drawer = document.querySelector('#drawer');
+  const drawerBtn = document.querySelector('#drawer-btn');
+  const restaurantElement = document.querySelector('#restaurants');
+  
+  menuBtn.addEventListener('click', function (event) {
+    drawer.classList.toggle('active');
+    event.stopPropagation();
+  });
 
+  drawerBtn.addEventListener('keypress', function (event) {
+    if (event.key === 'Enter') {
+      drawer.classList.toggle('active');
+    } 
+  });
 
-hero.addEventListener('click', function () {
-  drawer.classList.remove('active');
-});
+  
+  hero.addEventListener('click', function () {
+    drawer.classList.remove('active');
+  });
+  
+  main.addEventListener('click', function () {
+    drawer.classList.remove('active');
+  });
 
-main.addEventListener('click', function () {
-  drawer.classList.remove('active');
+  const renderRestaurants = async () => {
+    try {
+      const restaurants = await DataSource.fetchRestaurants();
+      const restaurantCards = restaurants.map(restaurant => {
+        return `
+
+          <article class="rest-item">
+            <div class="wraper">
+              <div class="rest-city">
+                <h1>${restaurant.city}</h1>
+              </div>
+              <img class="rest-item__thumb" src="${restaurant.pictureId}" alt="${restaurant.name}">
+              <div class="rest-item__content">
+                <h2>Rating ${restaurant.rating}</h2>
+                <h3 class="rest-item__title"><a href="#">${restaurant.name}</a></h3>
+                <p class="rest-item__description">${restaurant.description}</p>
+                <h1></h1>
+              </div>
+            </div>
+          </article>
+        `;
+      }).join('');
+      console.log(restaurantElement);
+      restaurantElement.innerHTML = restaurantCards;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  renderRestaurants();
+  
 });
