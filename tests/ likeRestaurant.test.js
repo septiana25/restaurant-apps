ad70f775b8d603e3999a014c621d334c3d6badb7
Ian Septiana <insaidseptiana@gmail.com>
2023-11-07 06:03:01 +0700

    test like button unlike

---
+++ tests/ likeRestaurant.test.js
@@ -0,0 +1,15 @@
+import LikeButtonInitiator from '../src/scripts/utils/like-button-initiator';
+
+describe('Liking A Restaurant', () => {
+  it('should show the like button when the restaurant has not been liked before', async () => {
+    document.body.innerHTML = '<div id="likeButtonContainer"></div>';
+
+    await LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      restaurant: {
+        id: 1,
+      },
+    });
+    expect(document.querySelector('[aria-label="like this movie"]')).toBeTruthy();
+  });
+});
